b6221b7bb9460f464040bf09ea508c462eb882cf
Douglas <japanmarsalis@gmail.com>
2023-06-02 18:06:50 -0500

    Everything works as should. Next, media queries.

--- Develop/script.js
+++ Develop/script.js
@@ -7,10 +7,10 @@ document.querySelector("#generate").addEventListener("click", beginPrompt);
 // Classes listed inside the HTML --> card, card-header, card-body, and card-footer: PROBABLY WON'T NEED
 
 //Arrays list
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",]
-var numberSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
-var specialCharact = [" ", "!", """, "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "|", "}", "~",]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var numberSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
+var specialCharact = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
 //Variable declarations list
 var confirmLength = "";
@@ -21,13 +21,15 @@ var confirmSpecialCharact;
 
 /*This is a loop for the Message Prompt for password length.  It will make sure that 
 the length falls within the limits of 8 - 128 characters*/
-function generate() {
-  var confirmLength = (prompt("Please enter a password character length between 8 and 128 characters."));
+// This generate function is a LONG FUNCTION.  It will finish AFTER the returnRandomPassword at the bottom of the page!!
+function generatePassword() {
+// This Message Prompt will ask the total number of characters the USER would like to have.
+var confirmLength = (prompt("Please enter a password character length between 8 and 128 characters.")); 
   while(confirmLength < 8 || confirmLength > 128) {
     alert("Sorry, the length MUST be between 8 and 128 characters!");
-    var length = (prompt("Please enter a password character length between 8 and 128 characters."));
+    var confirmLength = (prompt("Please enter a password character length between 8 and 128 characters."))
   }
-}
+
 
 // Message reports back how many charactes the USER will have.  
     alert(`Your password will have ${confirmLength} characters!`);
@@ -60,7 +62,7 @@ while(lowercase === false && uppercase === false && numberSet === false && speci
     }
 
     if (confirmNumberSet) {
-      passwordCharacters = passwordCharacters.concat(number)
+      passwordCharacters = passwordCharacters.concat(numberSet)
     }
 
     if (confirmSpecialCharact) {
@@ -77,6 +79,7 @@ var randomPassword = ""
         console.log(randomPassword)
       }
       return randomPassword;      
+}
 
 // This function will output the password to the screen.
 function beginPrompt() {
@@ -85,57 +88,3 @@ function beginPrompt() {
 // This line that will display to the screen.
   passwordText.value = password;
 }
-
-
-
-
-
-
-var length = numberSet(prompt("Please enter a password length between 8 and 128 characters.")),
-    characterType = prompt("Please enter a character type: Special, Number, Uppercase or Lowercase."),
-    password = generatePassword();
-
-   
-/* The generatePassword stores info in the password variable. The passwordText code is linked to 
-the password ID inside the HTML. */
-function generatePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-/*This code will have the user click on the button and the password will be written - the 
-writePassword can be found under the ABOVE function.*/
-generateBtn.addEventListener("click", writePassword);
-
-
-// Prompt for Length Question, Alert for wrong number, Question again, and Alert for telling the final number.
-//Prompt the user for the user criteria
-function generatePassword() {
-  var passwordLength = (prompt("How many characters would you like your password to contain (8-128 characters)?"));
-
-//Password is between 8 and 128
-  while(passwordLength >= 8 || passwordLength <= 128) {
-    alert("Password length must be between 8 to 128 characters. Please try again!");
-  var passwordLength = (prompt("How many characters would you like your password to contain (8-128 characters?"));
-  }
-
-//Make sure the input is validated
-    alert("Your password will contain ${confirmLength} characters.");
-
-// This will message the user about where their finished generated password will go.
-    return "Your generated password will go here.";
-} 
-   
-//Lowercase, uppercase, numbers and special characters
-
-for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * password.length);
-  password += character.substring(randomNumber, randomNumber +1);
-  }
-
-
-
-  // This is the line that displays on the screen.
-  passwordText.value = password;
-
-}
-
-
